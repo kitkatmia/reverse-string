1b5b8895d77f5d05a2344ca284a8727a69ff1a0d
JacksonOtto <73965396+JacksonO123@users.noreply.github.com>
2022-10-28 21:37:11 -0700

    best practices + reverse on input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { stdout } from 'process';
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState<string>('');
-  const [reversedString, setReversedString] = useState<string>("");
-  const handleUpdateValue = (e: any): void => {
+  const [reversedString, setReversedString] = useState<string>('');
+
+  const handleUpdateValue = (e: any) => {
     const input = e.currentTarget.value;
-    setInputValue(input);
+    setReversedString(reverseString(input));
   };
 
-  const reverseString = (): void => {
-    let reverseString = "";
-    for (let i = 0; i < inputValue.length; i++) {
-      reverseString = inputValue.charAt(i) + reverseString;
-    }
-    setReversedString(reverseString);
-  }
+  const reverseString = (str: string) => {
+    return str.split('').reverse().join('');
+  };
 
   return (
-    <div className="App">
+    <div className='app'>
       <h1>Hi. Enter in string and click button to reverse it.</h1>
-      <input onChange={handleUpdateValue} value={inputValue}></input>
-      <button onClick={reverseString}>Reverse String</button>
+      <input onChange={handleUpdateValue}></input>
       <p>{reversedString}</p>
     </div>
   );
